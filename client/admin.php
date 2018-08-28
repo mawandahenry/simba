f49bc7230d8b431e18d4bc5f3aaa1a2c82049006
Mawanda Henry <mawandahenrynaco@gmail.com>
2018-08-28 12:17:54 +0300

    version 1.0.0

---
+++ client/admin.php
@@ -0,0 +1,214 @@
+<html>
+	<head>
+		<title>Generic Page - Industrious by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<a class="logo" href="index.html">SIMBA SHIELD</a>
+				<nav>
+					<a href="#menu">Menu</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+			<h2>Admin</h2>
+				<ul class="links">
+					<li><a href="index.html">Add Guard</a></li>
+					<li><a href="elements.html">Delete Guard</a></li>
+					<li><a href="generic.html">Edit</a></li>
+					<li><a href="index.html">Add supervisor</a></li>
+					<li><a href="elements.html">Delete Sup</a></li>
+					<li><a href="generic.html">Edit Sup</a></li>
+				</ul>
+			</nav>
+
+
+
+<div id="heading" >
+<h1>SIMBA SHIELED</h1>
+<img src = "../assets/images/samban.png" width="220" height = "200" style="float:right;">
+</div></br>
+
+
+		<!-- Main -->
+<div class="add">
+   <h3 style="text-align:center;">Add Security Guard</h3></br>
+       <section>
+	   <form class="form-horizontal" role="form">
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" 
+placeholder="Enter Employee number">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Enter First Name">
+</div>
+</div>
+
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Enter Last Name">
+</div>
+</div>
+<div class="form-group col-md-5">
+<label for="name">Gender</label>
+<select class="form-control">
+<option>Male</option>
+<option>Female</option>
+
+</select>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Age">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="date of birth">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="NIN">
+</div>
+</div>
+<div class="form-group col-md-5">
+<label for="name">Status</label>
+<select class="form-control">
+<option>Single</option>
+<option>Married</option>
+
+</select>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Email">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Contact">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Emergency contact">
+</div>
+</div>
+<h4><u>Bank details</u> </h4>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Bank name">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Branch">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Account number">
+</div>
+</div>
+<h4><u>location</u></h4>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Home address">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="county">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Sub-county">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="village">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="District">
+</div>
+</div>
+<h4><u>INFO</u></h4>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Next of kin">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Contact">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-10">
+<input type="text" class="form-control" id="lastname"
+placeholder="Residence">
+</div>
+</div>
+<div class="form-group">
+<div class="col-sm-offset-2 col-sm-10">
+<button type="submit" class="btn btn-success">Add Guard</button>
+</div>
+</div>
+</form>
+</section>
+</div>
+
+		<!-- Footer -->
+			<footer id="footer" style="height:100px;">
+		<div class="content">
+    <section>
+	<h4>Simba Shield</h4>
+	   </section>
+		</div
+			</footer>
+
+		<!-- Scripts -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/js/queries.js"></script>
+			<script src="../assets/js/browser.min.js"></script>
+			<script src="../assets/js/breakpoints.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<script src="../assets/js/main.js"></script>
+
+	</body>
+</html>
